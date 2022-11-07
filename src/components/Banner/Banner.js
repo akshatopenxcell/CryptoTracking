6b12f101bf2a0386d86a044d744bbf3ff0000d8e
akshat.shah <akshat.shah@openxcell.com>
2022-11-07 17:43:23 +0530

    tracker

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-          OpenXcell Crypto Tracking App
+            Crypto Hunter
           </Typography>
           <Typography
             variant="subtitle2"
